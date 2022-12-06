fc1bbd1f353cdbcd17b16b229da4a2c2b8ecea68
Thomas Godwin <leddfoot36@gmail.com>
2022-12-06 10:01:15 +0100

    increased form accessibility

--- code/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
+++ code/02-adding-form-inputs/src/components/NewExpense/ExpenseForm.js
@@ -6,17 +6,17 @@ const ExpenseForm = () => {
   return (
     <form>
       <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='text' />
+        <div className='new-expense__control' id='expense-title'>
+          <label htmlFor='expense-title'>Title</label>
+          <input type='text' placeholder="enter title of the expense" />
         </div>
         <div className='new-expense__control'>
-          <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <label htmlFor='expense-amount'>Amount</label>
+          <input type='number' min='0.01' step='0.01' id='expense-amount'/>
         </div>
         <div className='new-expense__control'>
-          <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <label htmlFor='expense-date'>Date</label>
+          <input type='date' min='2019-01-01' max='2022-12-31' id='expense-date'/>
         </div>
       </div>
       <div className='new-expense__actions'>
